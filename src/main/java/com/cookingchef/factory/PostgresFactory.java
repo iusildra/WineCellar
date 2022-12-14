a3f0481ab1540ba36b62b011b40067298cfa38cd
Coco62442 <corentinclement10@gmail.com>
2022-12-14 12:07:16 +0100

    implements login usecase

---
+++ src/main/java/com/cookingchef/factory/PostgresFactory.java
@@ -0,0 +1,35 @@
+package com.cookingchef.factory;
+
+import com.cookingchef.dao.UserDAO;
+import com.cookingchef.dbutils.ConnectionManager;
+import com.cookingchef.facade.UserFacade;
+
+import java.sql.Connection;
+import java.util.Optional;
+
+public class PostgresFactory implements AbstractFactory {
+
+    private static volatile PostgresFactory instance;
+
+    private PostgresFactory() {}
+
+    public static synchronized PostgresFactory getPostgresFactory() {
+        if (instance == null) {
+            instance = new PostgresFactory();
+        }
+        return instance;
+    }
+    @Override
+    public UserDAO getUserDAO() {
+        // TODO : implements
+        return null;
+    }
+
+    @Override
+    public Optional<Connection> getPostgresFactory(String dbName, String user, String password, int port) {
+        return ConnectionManager.getConnection(dbName, user, password, port);
+    }
+
+
+}
+
