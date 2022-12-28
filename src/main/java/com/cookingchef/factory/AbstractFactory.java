68a1a2fcb3a92df13c06dcb93ab87d7e8f7073c3
Coco62442 <corentinclement10@gmail.com>
2022-12-28 18:33:05 +0100

    diagramm for recipe management

--- src/main/java/com/cookingchef/factory/AbstractFactory.java
+++ src/main/java/com/cookingchef/factory/AbstractFactory.java
@@ -3,5 +3,13 @@ package com.cookingchef.factory;
 import com.cookingchef.dao.UserDAO;
 
 public interface AbstractFactory {
+
+  /**
+   *
+   * @return
+   */
+  public static AbstractFactory getInstance() {
+    return PostgresFactory.getPostgresFactory();
+  }
   public UserDAO getUserDAO();
 }
