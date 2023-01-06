de443710b8481103910daba5ad7daf0cfd1bde82
alexis-fondard <alexis.fondard@etu.umontpellier.fr>
2023-01-06 16:56:53 +0100

    DAO recipe list

--- src/main/java/com/cookingchef/dao/Postgres/PostgresRecipeListDAO.java
+++ src/main/java/com/cookingchef/dao/Postgres/PostgresRecipeListDAO.java
@@ -1,7 +1,11 @@
 package com.cookingchef.dao.Postgres;
 
 import com.cookingchef.dao.RecipeListDAO;
+import com.cookingchef.model.RecipeList;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class PostgresRecipeListDAO implements RecipeListDAO {
@@ -22,4 +26,39 @@ public class PostgresRecipeListDAO implements RecipeListDAO {
     }
 
 
+    /**
+     * Creates a recipe list in the database
+     *
+     * @param userID     The ID of the current user.
+     * @param recipeList The RecipeList object that you want to create in the
+     *                   database.
+     * @return An optional integer.
+     */
+    @Override
+    public Optional<Integer> createRecipeListInDb(int userID, RecipeList recipeList) throws SQLException {
+        return Optional.empty();
+    }
+
+    /**
+     * Fetch a recipe list from the database by its ID.
+     *
+     * @param userID   The ID of the current user.
+     * @param recipeID The ID of the recipe list you want to get.
+     * @return An Optional object that contains a RecipeList object.
+     */
+    @Override
+    public Optional<RecipeList> getRecipeListById(int userID, int recipeID) throws SQLException {
+        return Optional.empty();
+    }
+
+    /**
+     * Returns a list of RecipeList objects that the user in the parameter has.
+     *
+     * @param userID The ID of the specified user.
+     * @return A list of recipe lists for the given user.
+     */
+    @Override
+    public List<RecipeList> getAllRecipeListByUserId(int userID) throws SQLException {
+        return null;
+    }
 }
--- src/main/java/com/cookingchef/dao/RecipeListDAO.java
+++ src/main/java/com/cookingchef/dao/RecipeListDAO.java
@@ -1,5 +1,47 @@
 package com.cookingchef.dao;
 
-public interface RecipeListDAO {
+import com.cookingchef.model.RecipeList;
+import com.cookingchef.model.Suggestion;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public interface RecipeListDAO {
+    /**
+     * Creates a recipe list in the database
+     *
+     * @param userID The ID of the current user.
+     * @param recipeList The RecipeList object that you want to create in the
+     *                   database.
+     * @return An optional integer.
+     */
+    Optional<Integer> createRecipeListInDb(int userID, RecipeList recipeList) throws SQLException;
+
+    /**
+     * Fetch a recipe list from the database by its ID.
+     *
+     * @param userID The ID of the current user.
+     * @param recipeID The ID of the recipe list you want to get.
+     * @return An Optional object that contains a RecipeList object.
+     */
+    Optional<RecipeList> getRecipeListById(int userID, int recipeID) throws SQLException;
+
+    /**
+     * Returns a list of RecipeList objects that the user in the parameter has.
+     *
+     * @param userID The ID of the specified user.
+     * @return A list of recipe lists for the given user.
+     */
+    List<RecipeList> getAllRecipeListByUserId(int userID) throws SQLException;
+
+    /**
+     * Fetch the favorite recipe list for the user from the database.
+     *
+     * @param userID The ID of the current user.
+     * @return An Optional object that contains a RecipeList object.
+     */
+    default Optional<RecipeList> getFavRecipeList(int userID) throws SQLException {
+        return getRecipeListById(userID, 0);
+    }
 }
